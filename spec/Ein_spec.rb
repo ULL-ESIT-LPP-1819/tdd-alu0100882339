05a2933d5d77589bcee431004b4adf2e8da688e4
arianezf <arianezf@gmail.com>
2018-11-07 18:02:24 +0000

    Fallo de la prueba 'Debe existir un nombre para la etiqueta'

--- spec/Ein_spec.rb
+++ spec/Ein_spec.rb
@@ -1,9 +1,20 @@
 require "spec_helper"
 
 RSpec.describe Ein do
-  it "Debe tener un constructor" do
-    expect(@etiqueta = EIN.new(8.400,70,20,260,90,50,6)).not_to be nil
-  end
+  
+  
+  describe EIN do
+    etiqueta = EIN.new(8.400,70,20,260,90,50,6)
+ 
+      context "Etiqueta_informacion_n" do
+  
+        it "Debe existir un nombre para la etiqueta" do
+          expect("Galleta").to eq(etiqueta.nombre)
+        end
+  
 
+      end
+  
+    end
  
 end
