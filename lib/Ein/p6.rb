bc0afb6c21358c2cff248a4f9c174dbd5cd9e84a
arianezf <arianezf@gmail.com>
2018-11-07 18:05:25 +0000

    Acierto en la prueba 'Debe existir un nombre para la etiqueta'

--- lib/Ein/p6.rb
+++ lib/Ein/p6.rb
@@ -1,7 +1,8 @@
 class EIN
-   
-   def initialize(v_energetico, grasa_t, grasa_s, hidratos_c, azucares, proteinas, sal)
+   attr_reader :nombre, :v_energetico, :grasa_t, :grasa_s, :hidratos_c, :azucares,:proteinas, :sal
+   def initialize(nombre,v_energetico, grasa_t, grasa_s, hidratos_c, azucares, proteinas, sal)
        
+       @nombre = nombre
        @v_energetico = v_energetico
        @grasa_t = grasa_t
        @grasa_s = grasa_s
--- spec/Ein_spec.rb
+++ spec/Ein_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Ein do
   
   
   describe EIN do
-    etiqueta = EIN.new(8.400,70,20,260,90,50,6)
+    etiqueta = EIN.new("Galleta",8.400,70,20,260,90,50,6)
  
       context "Etiqueta_informacion_n" do
   
