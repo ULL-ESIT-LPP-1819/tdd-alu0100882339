c837d08860e366e8655b03f4683d3bacbdca6dd2
arianezf <arianezf@gmail.com>
2018-11-07 18:09:38 +0000

    Acierto en la prueba 'Debe existir la cantidad de grasas'

--- lib/Ein/p6.rb
+++ lib/Ein/p6.rb
@@ -1,5 +1,5 @@
 class EIN
-   attr_reader :nombre
+   attr_reader :nombre, :grasa_t
    def initialize(nombre,v_energetico, grasa_t, grasa_s, hidratos_c, azucares, proteinas, sal)
        
        @nombre = nombre
