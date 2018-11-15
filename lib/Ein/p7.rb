84a8eb566037824fa13e038a6ec5e95808cc92f2
arianezf <arianezf@gmail.com>
2018-11-15 18:07:46 +0000

    Acierto en la prueba 'Debe existir un tail'

--- lib/Ein/p7.rb
+++ lib/Ein/p7.rb
@@ -1,12 +1,12 @@
 Node = Struct.new(:value, :next, :prev)
 
 class Lista 
-   attr_accessor :head #, :tail
+   attr_accessor :head, :tail
     
     def initialize
         
        @head = nil
-   #     @tail = nil
+       @tail = nil
         
     end
     
