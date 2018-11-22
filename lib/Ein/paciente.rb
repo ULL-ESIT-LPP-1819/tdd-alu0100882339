b2a24fbbfbaf701a473e011a8def5c142bc3c3b0
arianezf <arianezf@gmail.com>
2018-11-22 17:31:09 +0000

    Acierto en la prueba 'Debe tener un método para clasificar el imc'

--- lib/Ein/paciente.rb
+++ lib/Ein/paciente.rb
@@ -17,5 +17,23 @@ class Paciente < Individuo
         
     end
     
+    def clasificar_imc
+        imc = calcular_imc
+        
+        if(imc < 18.5)
+           "Bajo peso -- Delgado"
+         elsif(imc >= 18.5 && imc <= 24.9)
+            "Adecuado -- Aceptable"
+         elsif(imc >= 25 && imc <=29.9)
+            "Sobrepeso -- Sobrepeso"
+         elsif(imc >= 30 && imc <= 34.9)
+            "Obesidad grado 1 -- Obesidad"
+         elsif(imc >= 35 && imc <= 39.9)
+            "Obesidad grado 2 -- Obesidad"
+         elsif(imc >= 40)
+            "Obesidad grado 3 -- Obesidad"
+        end
+    end
+    
 
 end
