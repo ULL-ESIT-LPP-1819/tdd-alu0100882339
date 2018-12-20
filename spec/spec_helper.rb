b4c85988c679edce8e2d40424866c3e8978e478b
arianezf <arianezf@gmail.com>
2018-12-20 23:03:40 +0000

    coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require "bundler/setup"
 require "Ein"
-require ’coveralls’
+require 'coveralls'
 Coveralls.wear!
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
